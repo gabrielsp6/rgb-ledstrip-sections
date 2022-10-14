be45229edcc38325a853a6ae2ddae084889a8270
Gabriel <gabrielpojar6@gmail.com>
2022-10-14 21:50:49 +0300

    Update rgb-ledstrip-sections.ino
    add section change, remove fade effect

--- rgb-ledstrip-sections.ino
+++ rgb-ledstrip-sections.ino
@@ -1,72 +1,79 @@
 
 #include <Adafruit_NeoPixel.h>
-#include <Ramp.h> 
-
-ramp red;  
-ramp blue;
 
 String myColor;
 
 #ifdef __AVR__
-#include <avr/power.h> 
+#include <avr/power.h>
 #endif
 
-#define PIN        6 
-#define NUMPIXELS 24 
+#define PIN        6
+#define NUMPIXELS 24
 Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 15 
+#define DELAYVAL 15
+
+String readString;
+String redCode, greenCode, blueCode;
+int r, g, b;
+int pos;
 
 void setup() {
- Serial.begin(9600);  
+  Serial.begin(9600);
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
 
-      
-  red.go(140, 1500);                          
-  blue.go(255, 1500);        
-  
-  strip.begin(); 
+  r = 0;
+  b = 0;
+  g = 0;
+  strip.clear();
+  strip.begin();
 }
 
 
 void loop() {
 
-  
- while (Serial.available()==0){
 
-}
-  myColor=Serial.readString();
- 
+  while (Serial.available()) {
+    delay(3);  //delay to allow buffer to fill
+    if (Serial.available() > 0) {
 
- 
-  while(blue.isFinished() == false && red.isFinished() == false) {
-
-    strip.show(); 
-    strip.setPixelColor(1, red.update(), 0, blue.update());
-
-    strip.fill(strip.Color(red.update(), 0, blue.update()), 0, 24);
+      readString = Serial.readString();
+    }
   }
-    blue.go(1,2500);
-    red.go(1,2500);
 
 
-  Serial.print("red   ");
-  Serial.println(red.update());
-  Serial.print("blue   ");
-  Serial.println(blue.update());
+
+  redCode = readString.substring(2 , 5);
+  greenCode = readString.substring(6, 9);
+  blueCode = readString.substring(10, 13);
+
+  pos = readString.substring(0 , 1).toInt();
+  r = redCode.toInt();
+  g = greenCode.toInt();
+  b = blueCode.toInt();
+  
+  Serial.println(pos);
+  Serial.println(r);
+  Serial.println(g);
+  Serial.println(b);
+
+
   delay(50);
-
-  if(blue.getValue() == 1) {
-    red.go(140, 1500);                          
-    blue.go(255, 1500);        
-  }
-
-
-  
-  
-  
+if(pos == 1) {
+  strip.fill(strip.Color(r,g,b), 0, 8);
+}else if(pos == 2) {
+  strip.fill(strip.Color(r,g,b), 8, 8);
+}else if(pos == 3) {
+strip.fill(strip.Color(r,g,b), 16, 8);
  
 }
+
+
+
+
+
+
  
-  
+
+}
