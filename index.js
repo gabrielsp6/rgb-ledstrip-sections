f665820e10e0da3ed261f054527eb3f51d70fb6a
gabrielsp6 <gabrielpojar6@gmail.com>
2022-12-25 10:05:15 +0200

    add second section

--- index.js
+++ index.js
@@ -9,10 +9,94 @@ const connectionString = 'http://192.168.0.103:80'
     } : null;
   }
 
-document.querySelector('.triangle').addEventListener('click', function() {
+  let colors = {
+    one: [255,255,255],
+    two: [255,255,255],
+    three: [255,255,255],
+    four: [255,255,255],
+    five: [255,255,255],
+    six: [255,255,255],
+  }
 
+document.querySelector('.triangle-one').addEventListener('click', function() {
     let currentColor = hexToRgb(document.getElementById('color-picker').value);
 
+    let requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          seg: [
+            {
+              start: 0,
+              stop: 9,
+              col: [
+                [currentColor.r, currentColor.g, currentColor.b, 255],
+                [currentColor.r, currentColor.g, currentColor.b, 255],
+                [currentColor.r, currentColor.g, currentColor.b, 255],
+              ],
+              brightness: 255
+            }
+          ]
+        })
+      };
+    this.style.fill = document.getElementById('color-picker').value;
+    console.log(hexToRgb(document.getElementById('color-picker').value))
+    console.log(currentColor.r)
+    colors.one = [currentColor.r, currentColor.g, currentColor.b]
+    fetch(`${connectionString}/json/state`, requestOptions)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+    console.log(colors)
+  });
+
+  document.querySelector('.triangle-two').addEventListener('click', function() {
+    let currentColor = hexToRgb(document.getElementById('color-picker').value);
+
+    let requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          seg: [
+            {
+              start: 0,
+              stop: 9,
+              col: [
+                [colors.one[0], colors.one[1], colors.one[2], 255],
+                [colors.one[0], colors.one[1], colors.one[2], 255],
+                [colors.one[0], colors.one[1], colors.one[2], 255],
+              ],
+              brightness: 255
+            },
+            {
+              start: 9,
+              stop: 18,
+              col: [
+                [currentColor.r, currentColor.g, currentColor.b, 255],
+                [currentColor.r, currentColor.g, currentColor.b, 255],
+                [currentColor.r, currentColor.g, currentColor.b, 255],
+              ],
+              brightness: 255
+            }
+
+          ]
+        })
+      };
+    this.style.fill = document.getElementById('color-picker').value;
+    console.log(hexToRgb(document.getElementById('color-picker').value))
+    console.log(currentColor.r)
+    colors.two = [currentColor.r, currentColor.g, currentColor.b]
+    fetch(`${connectionString}/json/state`, requestOptions)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+    console.log(colors)
+  });
+
+
+
+  document.querySelector('.triangle-one').addEventListener('click', function() {
+    let currentColor = hexToRgb(document.getElementById('color-picker').value);
     let requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
